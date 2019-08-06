37a2689de52036cf49a35e62e760e88250b12e0c
rbobko <rbobko70@gmail.com>
2019-08-06 12:54:44 +0200

    Lesson 22. Adding warning and success message components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import {ServerComponent} from './server/server.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { ServersComponent } from './servers/servers.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningMessageComponent,
+    SuccessMessageComponent
   ],
   imports: [
     BrowserModule,
